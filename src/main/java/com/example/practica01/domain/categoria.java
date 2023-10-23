08188432ee9b0c5f840d2c0cb44bec6cf29518eb
jafet <jafet@DESKTOP-U798VJ2>
2023-10-23 13:41:30 -0600

    firebase

---
+++ src/main/java/com/example/practica01/domain/categoria.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.practica01.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+/**
+ *
+ * @author jafet
+ */
+@Data
+@Entity
+@Table(name= "categoria")
+public class categoria {
+    
+}
